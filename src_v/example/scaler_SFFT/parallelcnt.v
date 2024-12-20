9d42f01e403373135e0327cd98742300fc75ed55
Alexander Peacock <147108392+Tealimes@users.noreply.github.com>
2024-12-20 17:55:15 -0500

    Update parallelcnt.v

--- src_v/example/scaler_SFFT/parallelcnt.v
+++ src_v/example/scaler_SFFT/parallelcnt.v
@@ -1,11 +1,9 @@
-//By Alexander Peacock
+//By Alexander Peacock, undergrad at UCF ECE
 //email: alexpeacock56ten@gmail.com
 `ifndef parallelcnt
 `define parallelcnt
 
 module parallelcnt (
-    input wire iClk,
-    input wire iRstN,
     input wire iA,
     input wire iB,
     output wire [1:0] PCout
