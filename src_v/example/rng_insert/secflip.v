640dcbd8bcd4f9bc1aa20213123a6e55110c3a09
Alexander Peacock <147108392+Tealimes@users.noreply.github.com>
2025-02-27 20:42:45 -0500

    Update secflip.v

--- src_v/example/rng_insert/secflip.v
+++ src_v/example/rng_insert/secflip.v
@@ -16,36 +16,39 @@ module rng_insert #(
     output out
     );
     
-    wire [BITWIDTH-1:0] half;
-    reg signed [BITWIDTH-1:0] cnt;
-    reg state;
-    wire signed [BITWIDTH-1:0] mult;
-    wire signed [BITWIDTH-1:0] target;
-    reg [BITWIDTH-1:0] cntBit;
-    wire [BITWIDTH-1:0] prob;
+    wire [BITWIDTH-1:0] half; //stores 0.5
+    reg signed [BITWIDTH-1:0] cnt; //counts flips gotten
+    reg state; //used to give output
+    wire signed [BITWIDTH-1:0] mult; //holds multiplication value
+    wire signed [BITWIDTH-1:0] target; //holds target amount of flips needed
+    reg [BITWIDTH-1:0] cntBit; //counts number of bits in window
+    wire [BITWIDTH-1:0] prob; //holds probability needed
     
-    wire polarity;
+    wire polarity; //determines polarity of probability needed
     assign polarity = (half > iProb);
     
     assign half = {1'b0,1'b1,{(FBITWIDTH-2){1'b0}}}; //gets 0.5
     assign prob = (polarity) ? half - iProb : iProb-half; //finds probability needed
-    assign mult = (prob << (iWINLOG2));  //probability multiplication with window length
-    assign target = mult >>> (FBITWIDTH-1); //acounts for the decimal points of probability
+    assign mult = (prob << (iWINLOG2)); //multiplication of window using shifting
+    assign target = mult >> (FBITWIDTH-1); //finds target by shifting to account for fractional bits
     
-    wire [BITWIDTH-1:0] winStart;
+    
+    wire [BITWIDTH-1:0] winStart; //used for start of windo
     assign winStart = iWindow-1;
     
+    wire check; //checks if a new window is about to start or if target itself is not 0.5
+    assign check = cntBit == 0 && target != 0;
     
     //Used for bit flipping logic
     always@(posedge iClk or negedge iRstN) begin
     //resets the process
         if(!iRstN) begin 
-            state <= 0;
+            state <= 0;  
         end else begin
             //if the program is enabled, otherwise disable
             if(iEn) begin
                 //if the counter doesn't meet the target flip amount or window length just ends
-                if(cnt != target | (cntBit == 0)) begin 
+                if(cnt != target | (check)) begin 
                     //if the sign is negative flip 1s, otherwise flip 0s;
                     if(polarity) begin
                        //used to insert 0
@@ -66,14 +69,9 @@ module rng_insert #(
     //Used for counter logic
     always@(posedge iClk or negedge iRstN) begin
         //resets the process
-        if(!iRstN) begin
+        if(!iRstN | iClr) begin
             cnt <= 0;
-            //if cleared, count is cleared
-            if(iClr) begin
-                cntBit <= winStart;
-            end else begin 
-                cntBit <= 0;
-            end 
+            cntBit <= 0;
         end else begin
             //if the program is enabled, otherwise disable
             if(iEn) begin
@@ -84,9 +82,9 @@ module rng_insert #(
                             //if the sign is negative count down, otherwise count up 
                             cnt <= cnt + !(polarity ^ iA);
                     end else begin
-                        //keeps flip count static until the end of the window
-                        if(cntBit == 0) begin
-                            cnt <= !(polarity ^ iA); //allows for flipping first bit
+                        //keeps flip count static until the end of the window, unless a new window starts
+                        if(check) begin
+                            cnt <= !(polarity ^ iA); //allows for flipping first bit in a window
                         end else begin
                             cnt <= cnt;
                         end
